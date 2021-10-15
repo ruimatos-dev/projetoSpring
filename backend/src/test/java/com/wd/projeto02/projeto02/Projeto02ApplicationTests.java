eafc8597b3a65f5e776ec311147f2bc7cdf1284e
Rui Matos <rui.microk@gmail.com>
2021-10-15 06:34:59 -0300

    Iniciando o projeto Spring Boot

---
+++ backend/src/test/java/com/wd/projeto02/projeto02/Projeto02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wd.projeto02.projeto02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
